9250cd51350ff3dc2cf1e92c68b6c11bc98bec59
AJ Markow <alexanderjmarkow@gmail.com>
2020-09-23 11:00:51 -0700

    add tests for rock paper scissors choice in player object.  Pass tests for initializing choices, win array, and hash.

--- lib/rockpaper.rb
+++ lib/rockpaper.rb
@@ -1,16 +1,24 @@
 class Player
   # attr_writer(:name)
   attr_reader(:name)
-  attr_accessor(:choice,:win,:scoretally)
+  attr_accessor(:choices,:win,:scoretally)
   
   def initialize(name)
     @name = name
+    # @choice = ""
+    @choices = {1 => "Rock", 2 => "Paper", 3 => "Scissors"}
+    @scoretally = 0
+    @win = []
   end
 
-  def name=(name)
+  def name=name
     @name =name
+  end
+
+  def choice_chooser()
+    # @choice = choice
   end
 end
 
 # @choices = ["rock", "paper", "scissors"].sample
-# name, choice, win?, score tally
+# name, choice, win?, score tallyh
--- spec/rockpaper_spec.rb
+++ spec/rockpaper_spec.rb
@@ -15,4 +15,26 @@ describe(Player) do
       expect(fuzzle.name()).to(eq("Fuzzle"))
     end
   end
+
+  describe ('#initialize') do 
+   it('stores the value of players choices') do
+    fuzzle = Player.new("Fuzzle") 
+    expect(fuzzle.choices()).to be_an_instance_of(Hash)
+    end 
+  end 
+
+  describe('#initialize') do
+    it('checks that choices are stored in the choices hash') do
+      fuzzle = Player.new("Fuzzle")
+      expect(fuzzle.choices.fetch(1)).to(eq("Rock"))
+    end
+  end
+
+  describe('#initialize') do
+    it('checks that win contains an array for true or false values') do
+      fuzzle = Player.new("Fuzzle")
+      expect(fuzzle.win).to(eq([]))
+    end
+  end
+
 end
